9c5319fc53b6aa212c12431e0d69b6dfb470ff84
mattmega4 <matthewhsingleton@matthews-macbook-pro.local>
2017-06-25 23:13:57 -0700

    better model

---
+++ Charitable/Cause.swift
@@ -0,0 +1,17 @@
+//
+//  Cause.swift
+//  Charitable
+//
+//  Created by Matthew Howes Singleton on 6/25/17.
+//  Copyright © 2017 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+
+class Cause: NSObject {
+    
+    var name: String?
+    var descriptionOfCause: String?
+    var associatedWith: [Organization]?
+    
+}
---
+++ Charitable/Organization.swift
@@ -0,0 +1,27 @@
+//
+//  Organization.swift
+//  Charitable
+//
+//  Created by Matthew Howes Singleton on 6/25/17.
+//  Copyright © 2017 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+import Contacts
+
+class Organization: NSObject {
+    
+    var name: String?
+    var descriptionOfOrg: String?
+    var causeItHelps: Cause?
+//    var phone: CNPhoneNumber?
+//    var website: URL?
+//    var email: CNMutableContact?
+//    var location: CNPostalAddress?
+
+    
+
+    
+    
+
+}
